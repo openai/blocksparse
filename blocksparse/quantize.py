245b43a49689975cfd24e0e0773587ef3322beae
Scott Gray <scott@openai.com>
2018-09-07 13:08:21 -0700

    added missing time import

--- blocksparse/quantize.py
+++ blocksparse/quantize.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os.path
+import time
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
